4162a42fe7527cf6718a8cc46b0aabd859b0b9d6
ILIA <brakerilya@yandex.ru>
2025-11-01 12:13:49 +0300

    create add new contact positive test

--- src/main/java/pages/ContactsPage.java
+++ src/main/java/pages/ContactsPage.java
@@ -1,10 +1,13 @@
 package pages;
 
+import dto.Contact;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+
+import java.util.List;
 
 public class ContactsPage extends BasePage{
     public ContactsPage(WebDriver driver){
@@ -17,6 +20,8 @@ public class ContactsPage extends BasePage{
     WebElement btnContactsHeader;
     @FindBy(xpath = "//div[@class='contact-page_message__2qafk']")
     WebElement divTextNoContacts;
+    @FindBy(className = "contact-item_card__2SOIM")
+    List<WebElement> contactList;
 
     public boolean isTextContactsPresent(String text) {
         return isTextInElementPresent(btnContactsHeader,text);
@@ -25,5 +30,21 @@ public class ContactsPage extends BasePage{
     public boolean isTextNoContactsPresent(String text) {
         System.out.println(divTextNoContacts.getText());
         return isTextInElementPresent(divTextNoContacts, text);
+    }
+    public int getNumberOfContacts(){
+//        for (WebElement element:contactList){
+//            System.out.println(element.getText());
+//        }
+        return contactList.size();
+    }
+    public boolean isContactPresent(Contact contact){
+        for(WebElement element:contactList){
+            if(element.getText().contains(contact.getName())
+                    && element.getText().contains(contact.getPhone())){
+                System.out.println(element.getText());
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/ui_tests/AddNewContactTests.java
+++ src/test/java/ui_tests/AddNewContactTests.java
@@ -1,10 +1,13 @@
 package ui_tests;
 
+import dto.Contact;
 import dto.User;
 import manager.ApplicationManager;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.*;
+import utils.ContactFactory;
 import utils.HeaderMenuItem;
 
 import static pages.BasePage.*;
@@ -14,6 +17,7 @@ public class AddNewContactTests extends ApplicationManager {
     LoginPage loginPage;
     ContactsPage contactsPage;
     AddPage addPage;
+    int numberOfContacts;
 
     @BeforeMethod
     public void login(){
@@ -22,10 +26,19 @@ public class AddNewContactTests extends ApplicationManager {
         User user =new User("a@mail.ru","Password123!");
         loginPage.typeLoginFormWithUser(user);
         contactsPage=new ContactsPage(getDriver());
+        numberOfContacts=contactsPage.getNumberOfContacts();
         addPage=clickButtonHeader(HeaderMenuItem.ADD);
     }
     @Test
     public void addNewContactPositiveTest(){
-
+        addPage.typeContactForm(ContactFactory.positiveContact());
+        int numberOfContactsAfterAdd=contactsPage.getNumberOfContacts();
+        Assert.assertEquals(numberOfContactsAfterAdd,numberOfContacts+1);
+    }
+    @Test
+    public void addNewContactPositiveTestValidateList(){
+        Contact contact=ContactFactory.positiveContact();
+        addPage.typeContactForm(contact);
+       Assert.assertTrue(contactsPage.isContactPresent(contact));
     }
 }
